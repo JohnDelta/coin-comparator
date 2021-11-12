9c46b8c71927eb70b1cc38c342134ad72c478b8c
John Deligiannis <john.deligiannis1@gmail.com>
2021-11-12 22:46:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const Menu = () => {
+
+  const API_KEY = '';
+
+  const [coins, setCoins] = useState([]);
+  
+  const [InputCoin, setInputCoin] = useState('');
+
+  useEffect(() => {
+    setCoins(coins);
+  }, [coins]);
+
+  const OnInputCoinChange = (e) => {
+    let coin = e.target.value;
+    setInputCoin(coin);
+  };
+  
+  const AddCoin = () => {
+    if (InputCoin === '') return;
+    let updatedCoins = coins;
+    updatedCoins.push(InputCoin);
+    setCoins(updatedCoins);
+  };
+  
+  const RemoveCoin = () => {
+    if (InputCoin === '') return;
+    let updatedCoins = coins.filter((item) => item !== InputCoin);
+    setCoins(updatedCoins);
+  };
+  
+  const ClearCoins = () => {
+    setCoins([]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='menu-container'>
+      <div className='control-container'>
+        <p className='title'>Input Coin</p>
+        <input type='text' placeholder='ETH' onChange={(e) => {OnInputCoinChange(e)}} />
+        <button className='add-button' onClick={() => {AddCoin()}}>Add</button>
+        <button className='remove-button' onClick={() => RemoveCoin()}>Remove</button>
+      </div>
+      <div className='coins-container'>
+        <p className='title'>Saved Coins</p>
+        <div className='coins'>
+          {coins.map((coin, index) => {
+            return (
+              <div key={'coin_'+index} className='coin'>{coin}</div>
+            );
+          })}
+        </div>
+        <button className='clear-button' onClick={() => {ClearCoins()}}>Clear</button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <Menu />
     </div>
   );
 }
